de05c5b118cf3d51187c8b4f419fdb1fa2ab17a1
Felipe <felipe.londono.zuluaga@gmail.com>
2025-03-03 13:13:59 -0500

    Primer commit: Subiendo el programa inicial del trabajo final de POO 1
    Initial commit
    
    Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   README.md
    	new file:   bin/App.class
    	new file:   bin/Ejercicio1.class
    	new file:   bin/Ejercicio2.class
    	new file:   src/App.java
    	new file:   src/Ejercicio1.java
    	new file:   src/Ejercicio2.java

---
+++ src/Ejercicio1.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Ejercicio1 {
+    // Conversor de divisas tener en cuenta usar siempre la coma como punto decimal,
+    // por cuestiones regionales
+    public static void main(String[] args) {
+        Boolean funcionando = true;
+        Scanner consulta = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese la moneda que necesita cambiar: ");
+            System.out.println("a. => USD Dolares Norteamericanos");
+            System.out.println("b. => EUR Euros");
+            System.out.println("c. => JPY Yenes");
+            System.out.println("d. => GBP Libras Esterlinas");
+            System.out.println("e. => CHF Franco Suizo");
+            System.out.println("f. => RUB Rublo Ruso");
+            System.out.println("q. => Cerrar programa");
+
+            String moneda = consulta.nextLine().trim();
+            if (!moneda.equals("q")) {
+                double cantidad = 0;
+                System.out.println("Cuantos pesos desea cambiar?: ");
+                while (!consulta.hasNextDouble()) {
+                    System.out.println("Por favor, ingrese un número válido para la cantidad.");
+                    consulta.next();
+                }
+
+                cantidad = consulta.nextDouble();
+                consulta.nextLine();
+                System.out.println("Ingrese la tasa de cambio actual: ");
+                double conversor = 0;
+                while (!consulta.hasNextDouble()) {
+                    System.out.println("Por favor, ingrese un número válido para la tasa de cambio.");
+                    consulta.next();
+                }
+
+                conversor = consulta.nextDouble();
+                consulta.nextLine();
+                DecimalFormat df = new DecimalFormat("#.##");
+                String resultado = df.format(cantidad * (1 / conversor));
+
+                switch (moneda) {
+                    case "a":
+                        System.out.println("La cantidad de Dolares a cambiar son " + " " + resultado);
+
+                        break;
+                    case "b":
+                        System.out.println("La cantidad de Euros a cambiar son " + " " + resultado);
+
+                        break;
+                    case "c":
+                        System.out.println("La cantidad de Yenes a cambiar son " + " " + resultado);
+
+                        break;
+                    case "d":
+                        System.out.println("La cantidad de Libras a cambiar son " + " " + resultado);
+
+                        break;
+                    case "e":
+                        System.out.println("La cantidad de Francos Suizos a cambiar son " + " " + resultado);
+
+                        break;
+                    case "f":
+                        System.out.println("La cantidad de Rublos a cambiar son " + " " + resultado);
+
+                        break;
+
+                    default:
+                        System.out.println(
+                                "Opcion seleccionada no reconocida, por favor ingrese un valor de la (a) a la (f)");
+                        break;
+                }
+                System.out.println();
+            } else {
+                System.out.println("Cerrando programa...");
+                funcionando = false;
+            }
+
+        } while (funcionando);
+        consulta.close();
+
+    }
+}
